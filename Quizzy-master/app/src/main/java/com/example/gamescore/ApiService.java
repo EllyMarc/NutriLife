49d2b5b4acbd1b777ba92fe4f7d8e0436d7cfe08
Marcelly Portela <32777426+EllyMarc@users.noreply.github.com>
2026-09-24 20:13:01 -0400

    Correção de envios

--- Quizzy-master/app/src/main/java/com/example/gamescore/ApiService.java
+++
@@ -1,9 +0,0 @@
-package com.example.gamescore;
-import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Query;
-
-public interface ApiService {
-    @GET("api.php?type=multiple") //only multiple choice questions
-    Call<QuestionResponse> getQuestions(@Query("amount") int amount);
-}
